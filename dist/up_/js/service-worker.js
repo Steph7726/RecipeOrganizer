a38b5db18cd0ae197062f07356dbb786790444f2
folsteph-admin <s_pilon200892@fanshaweonline.ca>
2025-02-22 12:50:30 -0500

    fixes

---
+++ dist/up_/js/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "recipe-organizer-cache-v4";
+const FILES_TO_CACHE = [
+    "./html/index.html",
+    "./html/tasks.html",
+    "./css/style.css",
+    "./app.js",
+    "./manifest.json"
+];
+self.addEventListener("install", (event)=>{
+    event.waitUntil(caches.open(CACHE_NAME).then((cache)=>{
+        console.log("[Service Worker] Pre-caching offline content");
+        return cache.addAll(FILES_TO_CACHE);
+    }));
+    self.skipWaiting();
+});
+self.addEventListener("activate", (event)=>{
+    event.waitUntil(caches.keys().then((keyList)=>Promise.all(keyList.map((key)=>{
+            if (key !== CACHE_NAME) {
+                console.log("[Service Worker] Removing old cache:", key);
+                return caches.delete(key);
+            }
+        }))));
+    self.clients.claim();
+});
+self.addEventListener("fetch", (event)=>{
+    event.respondWith(caches.match(event.request).then((response)=>{
+        return response || fetch(event.request).catch(()=>caches.match("./html/index.html"));
+    }));
+});
+
+//# sourceMappingURL=service-worker.js.map
--- js/firebase.js
+++ js/firebase.js
@@ -16,3 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { provider };
